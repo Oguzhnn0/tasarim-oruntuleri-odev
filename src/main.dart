ee0df1367a08e288a3c86d8e6920191fee3cd301
Oğuzhan Arslan <121756813+Oguzhnn0@users.noreply.github.com>
2026-05-11 20:04:13 +0300

    Update main.dart

--- src/main.dart
+++ src/main.dart
@@ -1,1 +1,22 @@
-kodlar burada olacak
+class NotificationManager {
+  
+  void sendNotification (String tip, String mesaj, String alici){
+
+    if (tip == "E-mail"){
+      print("E-mail $alici kişisine gönderiliyor...");
+    }else if (tip == "SMS"){
+      print("SMS $alici kişisine gönderiliyor...");
+    }else{
+      print("Bilinmeyen tip!!!");
+    }
+
+  }
+}
+
+void main(){
+
+  var notification = NotificationManager();
+
+  notification.sendNotification("E-mail", "deneme 1 2 3", "ogz@example.com");
+  notification.sendNotification("SMS", "deneme 1 2 3", " 555 555 55 55");
+}
